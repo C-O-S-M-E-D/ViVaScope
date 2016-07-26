5b09e1be9842002949d7c1b8c806e28ee3b9fc74
A.M. Smith <smitham42@gmail.com>
2016-07-26 16:21:12 -0700

    Fixed

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>ServiceDog</h1>'
+  template: '<h1>ViVaScope</h1>'
 })
 export class AppComponent { }
